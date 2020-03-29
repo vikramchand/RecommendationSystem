c780769b891aac9c1e961751171fa5ad7023d09e
vikramchand <vikramchandvankar@gmail.com>
2020-03-29 14:48:26 -0700

    Skeleton App

---
+++ src/main/java/com/matcher/recommendations/MatcherRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.matcher.recommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatcherRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatcherRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matcher/recommendations/MatcherRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matcher.recommendations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatcherRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
